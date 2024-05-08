6e18737869edaf10de9df265473e9ae9db09f223
jmartinez419 <justynmartinez419@gmail.com>
2024-05-08 16:51:18 -0400

    first commit

---
+++ Florida/app.js
@@ -0,0 +1,8 @@
+const ourName = "Justyn"
+const visited = true
+const moneySpent = "a lot"
+
+
+console.log(
+    `It is a ${visited} statement that ${ourName} visited this state, and spent ${moneySpent} dollars.`
+);
---
+++ Virginia/app.js
@@ -0,0 +1,8 @@
+const ourName = "Justyn"
+const visited = false
+const moneySpent = 0
+
+
+console.log(
+    `It is a ${visited} statement that ${ourName} visited this state, and spent ${moneySpent} dollars.`
+);
